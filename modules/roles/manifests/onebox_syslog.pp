9644905923e5f7eb2970c6fee46295dbc7993b32
Jamie Riedesel <jamie.riedesel@hellosign.com>
2016-09-14 15:01:59 -0500

    Adds example of curator weekly purge.

--- modules/roles/manifests/onebox_syslog.pp
+++ modules/roles/manifests/onebox_syslog.pp
@@ -55,4 +55,18 @@ class roles::onebox_syslog {
     cron_minute => '05',
   }
 
+  # This is what a weekly index would look like.
+  # We're not using it here, this is just for example.
+  # curator::job { 'onebox_delete_weekly':
+  #   command      => 'delete',
+  #   prefix       => 'onebox-',
+  #   timestring   => '\%G.\%V',
+  #   time_unit    => 'weeks',
+  #   older_than   => '2',
+  #   cron_hour    => 1,
+  #   cron_minute  => 02,
+  #   cron_weekday => 3,
+  # }
+  # WARNING: curator uses python strftime for timestring, not joba!
+
 }
