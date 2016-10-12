a3c25b8e9da627f4a4dddcf18a733b801b1d4885
Jamie Riedesel <jamie.riedesel@hellosign.com>
2016-10-12 15:50:28 -0500

    Ensure onebox ES box has the correct logstash template

--- modules/profiles/manifests/logstash/output_onebox.pp
+++ modules/profiles/manifests/logstash/output_onebox.pp
@@ -1,6 +1,19 @@
 # Outputs to the local ES repo for onebox installs.
 class profiles::logstash::output_onebox {
 
+  # Because onebox uses an index named something other than 'logstash', we
+  # have to import our own template. This copies the template-file to the
+  # local file-system, and the configfile fragment will use that to 
+  # update the ES Cluster templates. New indices will get those mappings.
+
+  file { '/etc/logstash/logstash.json':
+    owner   => 'logstash',
+    group   => 'logstash',
+    source  => 'puppet:///modules/profiles/logstash/templates/onebox.json',
+    require => Logstash::Configfile ['output_onebox_es'],
+    notify  => Service['logstash'],
+  }
+
   logstash::configfile { 'output_onebox_es':
     template => 'profiles/logstash/output/onebox_es',
     order    => 50,
